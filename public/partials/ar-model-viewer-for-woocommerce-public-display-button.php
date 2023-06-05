1f651153478d80fda2109e80e81812b02be48ad5
ra_cm <ra_cm@outlook.com>
2023-06-04 19:26:03 -0600

    1.0.5

--- public/partials/ar-model-viewer-for-woocommerce-public-display-button.php
+++ public/partials/ar-model-viewer-for-woocommerce-public-display-button.php
@@ -14,7 +14,6 @@
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <button id="ar_model_viewer_for_woocommerce_btn">View in 3D</button>
-<?php echo $this->ar_model_viewer_for_woocommerce_ar_modes($ar_mode); ?>
 
 <div id="dialog" title="<?php echo $product->get_name(); ?>">
   <!-- AR Model Viewer for WooCommerce - Styles -->
