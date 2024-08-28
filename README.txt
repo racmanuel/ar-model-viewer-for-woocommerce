f6439a34d57a10d0076cb204c6629a3bb5b88bdb
Manuel Ramirez Coronel <ra_cm@outlook.com>
2024-08-27 19:20:31 -0600

    🚀 [1.0.7] Removed the Appsero SDK and add Freemius SDK, and Tested up to: 6.6 version of WordPress, removed uninstall.php, and on deactive plugin delete_option('ar_model_viewer_for_woocommerce_settings');
    [1.0.7] Removed the Appsero SDK and add Freemius SDK, and Tested up to: 6.6 version of WordPress, removed uninstall.php, and on deactive plugin delete_option('ar_model_viewer_for_woocommerce_settings');

--- README.txt
+++ README.txt
@@ -3,13 +3,13 @@ Contributors: racmanuel
 Donate link: https://racmanuel.dev 
 Tags: Augmented Reality, AR, Model Viewer, 3D, Woocommerce
 Requires at least: 5.9
-Tested up to: 6.5
-Stable tag: 1.0.5
+Tested up to: 6.6
+Stable tag: 1.0.7
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
-The AR Model Viewer for WooCommmerce is a plugin that allows you to show 3D models in your products of your website and allow people to view them in augmented reality. 3D model files such as.glb and.gltf are supported by this plugin. The plugin is incredibly simple to use.
+The AR Model Viewer for WooCommerce plugin shows 3D models on your website and in augmented reality. Supports .glb and .gltf files.
 
 == Description ==
 
@@ -30,15 +30,13 @@ If you need any customization for the plugin, feel free to send me a message or 
 
 ## Privacy Policy 🔒
 
-AR Model Viewer for WooCommerce uses the [Appsero](https://appsero.com) SDK to collect telemetry data, but only with the user's explicit consent. This data collection helps us troubleshoot issues and improve our product.
+AR Model Viewer for WooCommerce uses the Freemius SDK to collect telemetry data, but only with the user's explicit consent. This data collection helps us troubleshoot issues and improve our product.
 
 - **No data is gathered by default.**
 - Data collection only begins **after user consent via the admin notice**.
 - Collected data ensures a great user experience.
 
-Integrating the Appsero SDK **does not immediately start data collection without user confirmation**.
-
-For more details on how Appsero collects and uses data, please refer to their [privacy policy](https://appsero.com/privacy-policy/).
+Integrating the Freemius SDK **does not immediately start data collection without user confirmation**.
 
 == Installation ==
 
@@ -53,6 +51,11 @@ e.g.
 
 == Frequently Asked Questions ==
 
+= Which file types are required for the 3D models/scenes? =
+GLTF (*.glb) files are required for the 3D preview in the browser and the AR scenes on Android devices. Apple devices, such as iPhone, iPad or Apple Vision Pro, on the other hand, require **USDZ** (*.usdz) files to display AR content. AR Model Viewer for WooCommerce also adds support for **Reality** (*.reality) files for Apple devices.
+
+= Do I have to add models for Android and iOS? =
+To ensure that all visitors to your website can see the AR scenes, each scene must have a dedicated model in both formats (.glb, .usdz) for Android and iOS/Apple devices. However, only a GLTF file is required for the 3D preview in the browser.
 
 == Screenshots ==
 
@@ -60,6 +63,7 @@ e.g.
 == Changelog ==
 
 1.0.5 - Update the tested up to 6.5 and added Appsero
+1.0.6 - Update the tested up to 6.5 and added Appsero
+1.0.7 - Removed the Appsero SDK and add Freemius SDK, and Tested up to: 6.6 version of WordPress, removed uninstall.php, and on deactive plugin delete_option('ar_model_viewer_for_woocommerce_settings');
 
 == Upgrade Notice ==
-
--- ar-model-viewer-for-woocommerce.php
+++ ar-model-viewer-for-woocommerce.php
@@ -15,11 +15,11 @@
  * Plugin Name:       AR Model Viewer for WooCommerce
  * Plugin URI:        https://plugin.com/ar-model-viewer-for-woocommerce-uri/
  * Description:       AR Model Viewer for WooCommerce plugin is an all in one solution to allow you to present your 3D models in an interactive AR view directly in your browser on both iOS and Android devices and all the products you have a 3D model, this plugin support formats .glb
- * Version:           1.0.5
+ * Version:           1.0.7
  * Author:            Manuel Ramirez Coronel
  * Requires at least: 5.9
  * Requires PHP:      7.4
- * Tested up to:      6.5
+ * Tested up to:      6.6
  * Requires Plugins: woocommerce
  * WC requires at least: 3.9
  * WC tested up to: 8.8
@@ -28,6 +28,8 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       ar-model-viewer-for-woocommerce
  * Domain Path:       /languages
+ *
+ * @fs_premium_only /js/ar-model-viewer-for-woocommerce-admin-ctp.js, /js/ar-model-viewer-for-woocommerce-admin-ctp-dist.js, /css/ar-model-viewer-for-woocommerce-admin-ctp.css, /admin/class-ar-model-viewer-for-woocommerce-admin-ajax.php, /admin/class-ar-model-viewer-for-woocommerce-admin-custom-post-type.php
  */
 
 // If this file is called directly, abort.
@@ -42,12 +44,51 @@ if (!defined('WPINC')) {
  */
 require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
 
+if (!function_exists('ar_model_viewer_for_woocommerce_fs')) {
+    // Create a helper function for easy SDK access.
+    function ar_model_viewer_for_woocommerce_fs()
+    {
+        global $ar_model_viewer_for_woocommerce_fs;
+
+        if (!isset($ar_model_viewer_for_woocommerce_fs)) {
+            // Include Freemius SDK.
+            require_once dirname(__FILE__) . '/vendor/freemius/wordpress-sdk/start.php';
+
+            $ar_model_viewer_for_woocommerce_fs = fs_dynamic_init(array(
+                'id' => '16088',
+                'slug' => 'ar-model-viewer-for-woocommerce',
+                'type' => 'plugin',
+                'public_key' => 'pk_5143076d3ed4661ac299aa66baabc',
+                'is_premium' => true,
+                'premium_suffix' => 'Pro',
+                // If your plugin is a serviceware, set this option to false.
+                'has_premium_version' => true,
+                'has_addons' => false,
+                'has_paid_plans' => true,
+                'menu' => array(
+                    'slug' => 'ar_model_viewer_for_woocommerce_settings',
+                    'parent' => array(
+                        'slug' => 'options-general.php',
+                    ),
+                ),
+            ));
+        }
+
+        return $ar_model_viewer_for_woocommerce_fs;
+    }
+
+    // Init Freemius.
+    ar_model_viewer_for_woocommerce_fs();
+    // Signal that SDK was initiated.
+    do_action('ar_model_viewer_for_woocommerce_fs_loaded');
+}
+
 /**
  * Current plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('AR_MODEL_VIEWER_FOR_WOOCOMMERCE_VERSION', '1.0.5');
+define('AR_MODEL_VIEWER_FOR_WOOCOMMERCE_VERSION', '1.0.7');
 
 /**
  * Define the Plugin basename
@@ -81,32 +122,30 @@ function ar_model_viewer_for_woocommerce_deactivate()
 register_activation_hook(__FILE__, 'ar_model_viewer_for_woocommerce_activate');
 register_deactivation_hook(__FILE__, 'ar_model_viewer_for_woocommerce_deactivate');
 
+// Not like register_uninstall_hook(), you do NOT have to use a static function.
+ar_model_viewer_for_woocommerce_fs()->add_action('after_uninstall', 'ar_model_viewer_for_woocommerce_uninstall');
+
+function ar_model_viewer_for_woocommerce_uninstall()
+{
+
+    if (!defined('WP_UNINSTALL_PLUGIN')
+        || empty($_REQUEST)
+        || !isset($_REQUEST['plugin'])
+        || !isset($_REQUEST['action'])
+        || 'ar-model-viewer-for-woocommerce/ar-model-viewer-for-woocommerce.php' !== $_REQUEST['plugin']
+        || 'delete-plugin' !== $_REQUEST['action']
+        || !check_ajax_referer('updates', '_ajax_nonce')
+        || !current_user_can('activate_plugins')
+    ) {
+        exit;
+    }
+}
+
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
 require plugin_dir_path(__FILE__) . 'includes/class-ar-model-viewer-for-woocommerce.php';
-
-/**
- * Initialize the plugin tracker
- *
- * @return void
- */
-function appsero_init_tracker_ar_model_viewer_for_woocommerce()
-{
-
-    if (!class_exists('Appsero\Client')) {
-        require_once __DIR__ . '/appsero/src/Client.php';
-    }
-
-    $client = new Appsero\Client('25337288-b833-4220-ad60-666a2ae90aac', 'AR Model Viewer for WooCommerce', __FILE__);
-
-    // Active insights
-    $client->insights()->init();
-
-}
-
-appsero_init_tracker_ar_model_viewer_for_woocommerce();
 
 /**
  * Begins execution of the plugin.
